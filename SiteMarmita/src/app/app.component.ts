f3e41fd16bc771c281764b299bf30cc12d40cc50
Guilherme-Geraldo2106 <guilherme.geraldo2106@gmail.com>
2023-12-24 16:03:17 -0300

    Correção arquitetura das pastas dos componentes, e correção da instalação do bootstrap

--- SiteMarmita/src/app/app.component.ts
+++ SiteMarmita/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FoodsComponent } from './foods/foods.component';
+import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+            RouterOutlet,
+            FoodsComponent,
+            IngredientListComponent,
+            HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
