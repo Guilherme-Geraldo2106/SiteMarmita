745c183718d0fa7a02e17267a8d163dbba51b8e0
Guilherme-Geraldo2106 <guilherme.geraldo2106@gmail.com>
2023-12-25 20:14:47 -0300

    preenchimento listas de marmitas, e ingredientes

--- SiteMarmita/src/app/foods/food-list/food-list.component.ts
+++ SiteMarmita/src/app/foods/food-list/food-list.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FoodItemComponent } from './food-item/food-item.component';
 //Models
-import { Food } from './food.model';
+import { Food } from '../food.model';
 
 @Component({
   selector: 'app-food-list',
   standalone: true,
-  imports: [FoodItemComponent],
+  imports: [FoodItemComponent, CommonModule],
   templateUrl: './food-list.component.html',
   styleUrl: './food-list.component.css'
 })
 
 export class FoodListComponent implements OnInit {
-  foods: Food[] = [new Food("Mini", "marmita tamanho mini", "https://marmitexdesucesso.com/wp-content/uploads/2019/10/qual-a-quantidade-de-comida-para-marmitex-1200x675.jpg"),
-  new Food("Media", "marmita tamanho medio", "https://www.takenteembalagens.com.br/imagens/informacoes/embalagem-mini-marmita-aluminio-04.jpg")];
+  foods: Food[] = [new Food("Parmegiana de alcatra", "marmita de parmegiana de alcatra, acompanha fritas", "https://srantonini.com.br/wp-content/uploads/2022/12/24-1.png"),
+  new Food("Parmegiana de frango", "marmita de parmegiana de frango, acompanha fritas", "https://srantonini.com.br/wp-content/uploads/2022/12/42-1.png")];
 
   constructor() {
 
--- SiteMarmita/src/app/ingredient-list/ingredient-list.component.ts
+++ SiteMarmita/src/app/ingredient-list/ingredient-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//Component
 import { IngredientListEditComponent } from './ingredient-list-edit/ingredient-list-edit.component';
+//Models
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-ingredient-list',
   standalone: true,
-  imports: [IngredientListEditComponent],
+  imports: [IngredientListEditComponent, CommonModule],
   templateUrl: './ingredient-list.component.html',
   styleUrl: './ingredient-list.component.css'
 })
-export class IngredientListComponent {
+export class IngredientListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient("Alcatra", 10),
+    new Ingredient("Frango", 10),
+    new Ingredient("Batata", 20)
+  ]
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
